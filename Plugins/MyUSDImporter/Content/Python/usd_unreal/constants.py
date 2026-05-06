12ea8799a9c8e9b29db74014e78aff811fc22d84
Yujiecong <424586730@qq.com>
2026-05-06 23:20:57 +0800

    。

--- Plugins/MyUSDImporter/Content/Python/usd_unreal/constants.py
+++
@@ -1,4 +0,0 @@
-LOD_VARIANT_SET_TOKEN = 'LOD'
-
-# DEPRECATED: RootPrimName is now a Level / LevelSequence export option
-ROOT_PRIM_NAME = 'Root'
--- Plugins/MyUSDImporter/Source/MyUSDSchemas/Public/MyUSDSchemaTranslator.h
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#pragma once
-
-#include "Misc/CoreMiscDefines.h"
-UE_DEPRECATED_HEADER(5.6, "Use Objects/USDSchemaTranslator.h from the USDUtilities module instead")
-
-#include "Objects/USDSchemaTranslator.h"
